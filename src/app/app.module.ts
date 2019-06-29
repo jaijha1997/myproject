2d7e663d64eb5a3e7c02709a75cda3e8d272a0a4
jaijha1997 <jayjha!997@gmail.com>
2019-06-29 21:07:25 +0530

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotpwComponent } from './forgotpw/forgotpw.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetpwComponent } from './resetpw/resetpw.component';
+
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    OrderComponent,
+    LoginComponent,
+    ForgotpwComponent,
+    SignupComponent,
+    ResetpwComponent,
+    AdmindashboardComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
